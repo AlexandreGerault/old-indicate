b1ec7d6fd1b088e02f153d983940a171df160245
Alexandre Gérault <gerault-alexandre@orange.fr>
2019-11-12 13:03:02 +0100

    Adding fields

---
+++ resources/lang/fr/structure/rating.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Ratings criteria
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the structure's data names.
+    |
+    */
+    'comment' => 'commentaire',
+
+    // COMPANY
+    'skills' => 'compétences',
+    'expertise' => 'savoir-faire',
+    'market' => 'marché',
+    'advantage_designed' => 'avantage conçu',
+    'team' => 'équipe',
+    'shareholding_created' => 'chiffre d\'affaire réalisé',
+    'input_barrier' => 'barrière à l\'entrée',
+    'value_creation' => 'valorisation',
+
+    // CONSULTING & INVESTOR
+    'support_quality' => 'qualité des supports',
+    'procedure_simplicity' => 'simplicité de la procédure',
+    'procedure_speed' => 'vitesse de la procédure',
+    'global_rating' => 'note globale de la structure'
+
+];
