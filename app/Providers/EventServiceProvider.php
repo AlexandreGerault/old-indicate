00b4209872363cf714d0fb5b38488210f3c448dd
Alexandre Gérault <gerault-alexandre@orange.fr>
2019-11-12 13:08:33 +0100

    Handle claim demands

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\ValidatedClaimDemand;
+use App\Listeners\OnValidatedClaimDemand;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -34,6 +36,9 @@ class EventServiceProvider extends ServiceProvider
         ],
         NewsDeleted::class => [
         ],
+        ValidatedClaimDemand::class => [
+            OnValidatedClaimDemand::class,
+        ]
 
     ];
 
