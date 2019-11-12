9ee64c951ffb5eca8a3f4ae9575336acbee8e4a1
Alexandre Gérault <gerault-alexandre@orange.fr>
2019-11-12 13:06:28 +0100

    Base view to manage claiming demands

--- resources/views/backoffice/claim_demand/index.blade.php
+++ resources/views/backoffice/claim_demand/index.blade.php
@@ -7,10 +7,10 @@
     @foreach($claimdemands as $demand)
     <tr>
         <td><a href="{{ route('user.show', ['user' => $demand->user]) }}">{{ $demand->user->firstname . ' ' . $demand->user->lastname }}</a></td>
-        <td><a href="{{ route('structure.show', ['structure' => $demand->$structure]) }}">{{ $demand->structure->name }}</a></td>
+        <td><a href="{{ route('structure.show', ['structure' => $demand->structure]) }}">{{ $demand->structure->name }}</a></td>
         <td>
-            <a class="btn btn-primary" href="{{ route('claimdemand.validates', ['claimdemand' => $demand]) }}">Accepter</a>
-            <a class="btn btn-primary" href="{{ route('claimdemand.rejects', ['claimdemand' => $demand]) }}">Refuser</a></td>
+            <a class="btn btn-primary" href="{{ route('claimdemand.validates', ['user_id' => $demand->user->id, 'structure_id' => $demand->structure->id]) }}">Accepter</a>
+            <a class="btn btn-primary" href="{{ route('claimdemand.rejects', ['user_id' => $demand->user->id, 'structure_id' => $demand->structure->id]) }}">Refuser</a></td>
     </tr>
     @endforeach
 </table>
